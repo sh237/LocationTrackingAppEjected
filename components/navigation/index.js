86ee019725fa4d5a0da9578f54be33e27c3375ed
Shunya Nagashima <syun864297531@gmail.com>
2022-09-22 23:49:19 +0900

    ナビゲーションバーのボタンの追加

--- components/navigation/index.js
+++ components/navigation/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StyleSheet,TouchableOpacity,Text,Button } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import MapDisplay from '../map/MapDisplay';
@@ -10,16 +11,55 @@ import Login from '../authentication/Login';
 import Register from '../authentication/Register';
 import Home from '../authentication/Home';
 
+
+// import {LogBox} from "react-native";
+// LogBox.ignoreLogs(['new NativeEventEmitter']); // Ignore log notification by message
+// LogBox.ignoreAllLogs(); //Ignore all log notifications
+
 const RootStack = createStackNavigator();
 
 const RootStackScreen = () => {
+  const styles = StyleSheet.create({
+    header: {
+      borderBottomWidth: 5,
+      borderBottomColor: 'blue',
+   
+    },
+    button: {
+      alignItems: 'center',
+      backgroundColor: '#DDDDDD',
+      padding: 10,
+    },
+  });
   return (
     <NavigationContainer>
       <RootStack.Navigator>
         <RootStack.Screen name="Login" component={Login} />
-        <RootStack.Screen name="Calendar" component={CalendarDisplay} />
+        <RootStack.Screen
+         name="Calendar" component={CalendarDisplay} screenOptions={{ headerStyle: styles.header }}
+         options={{
+          // headerTitle: (props) => <LogoTitle {...props} />,
+          // headerTitle: "" ,
+          headerRight: () => (
+            <TouchableOpacity style={this.button}>
+            <Text >aaa</Text>
+          </TouchableOpacity>
+            // <Button
+            //   onPress={() => alert('This is a button!')}
+            //   title="Info"
+            //   // color="#fff"
+            //   style={styles.button}
+            // />
+          ),
+        }}/>
         <RootStack.Screen name="Map" component={MapDisplay} />
-        <RootStack.Screen name="TodayMap" component={TodayMapDisplay} />
+        <RootStack.Screen name="TodayMap" component={TodayMapDisplay} 
+              options={({ navigation,route })=>({
+          headerLeft: (props) => (
+            <Button title="Calendar" onPress={()=>{navigation.navigate("Calendar",{user: route.params.user, date: route.params.date})}}>Calendar</Button>
+          ),
+        })}
+  />
         <RootStack.Screen name="Tracking" component={WatchLocation} />
         <RootStack.Screen name="Register" component={Register} />
         <RootStack.Screen name="Home" component={Home} />

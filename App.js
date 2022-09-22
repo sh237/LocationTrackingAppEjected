913094086d649f924d9d3fcb4a6b927fcc6f5f21
Shunya Nagashima <syun864297531@gmail.com>
2022-09-22 23:49:49 +0900

    細かい調整

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ function App (){
   useEffect(() => {
     axios.defaults.baseURL = 'http://localhost:8000';
     // axios.defaults.baseURL = 'http://127.0.0.1:8000/';
-    axios.defaults.timeout = 1500;
+    axios.defaults.timeout = 3000;
     if (Platform.OS === 'ios') {
       Geolocation.requestAuthorization('always');
     }

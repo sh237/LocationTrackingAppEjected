0ef99cd83cb7532b2b8ed332fd5643156405eff9
Shunya Nagashima <syun864297531@gmail.com>
2022-09-27 23:24:27 +0900

    スケジュール追加画面の作成

--- components/calendar/CalendarDisplay.js
+++ components/calendar/CalendarDisplay.js
@@ -1,11 +1,166 @@
-import React,{useState} from 'react'
-import {StyleSheet, View, Text, Button} from 'react-native';
+import React,{useState, useEffect} from 'react'
+import {StyleSheet, View, Text, Button,TouchableOpacity,AppState} from 'react-native';
 import { Calendar,LocaleConfig } from 'react-native-calendars';
 import moment from "moment";
+import axios from 'axios';
+import {Moment} from 'moment';
+import Geolocation from 'react-native-geolocation-service';
+import BackgroundGeolocation from "react-native-background-geolocation";
 
 const INITIAL_DATE = moment().format("YYYY-MM-DD");
 
 const CalendarDisplay = ({navigation, route}) => {
+    const [currentDate, setCurrentDate] = useState(moment());
+    const [appState, setAppState] = useState(AppState.currentState);
+    const [calendarid, setCalendarid] = useState(0);
+
+    const handleAppStateChange = (nextAppState) => {
+      let subscription = null;
+      console.log('app State: ' + appState+' next app State: '+nextAppState);
+      if (appState != nextAppState) {
+        if(nextAppState.match(/inactive|background/)){
+          console.log(
+            'App State: ' +
+            'App has come to the background!'
+          );
+          // alert(
+          //   'App State: ' +
+          //   'App has come to the background!'
+          // );
+          console.log("back"+subscription)
+          if(subscription == null){
+            subscription = BackgroundGeolocation.onLocation(onLocation, onError);
+          }
+        }
+        console.log("set")
+
+        // alert('App State: ' + nextAppState);
+        setAppState(nextAppState);
+      }
+      if (nextAppState === "active") {
+        console.log(
+          'App State: ' +
+          'App has come to the foreground!'
+        );
+        // alert(
+        //   'App State: ' +
+        //   'App has come to the foreground!'
+        // );
+        console.log("fore"+subscription);
+        // if (calendarid != 0) {
+        //   console.log("timeout");
+        //   const timer = setTimeout(Geolocation.getCurrentPosition(
+        //     position => {
+        //       const {latitude, longitude} = position.coords;
+        //       const payload = { calendar: calendarid, mpoint:"MULTIPOINT ("+longitude+" "+latitude+")"};
+        //       axios.put(`/api/location/update/${calendarid}`,payload).then(response => {
+        //         console.log("updated location");
+        //       }).catch(error => console.log("post error:::"+error));
+        //     },
+        //     error => {
+        //       console.log(error.code, error.message);
+        //     },
+        //     {enableHighAccuracy: false, timeout: 15000, maximumAge: 10000},
+        //   ), 60 * 1000);
+        //   }
+      }
+    };
+
+    useEffect(() => {
+      console.log("CalendarDisplay.js"+new Date(new Date() -  new Date().getTimezoneOffset() * 60 * 1000).toISOString().split('T')[0]);
+      axios
+      .get(`/api/calendar/${route.params.user}/?search=${new Date(new Date() -  new Date().getTimezoneOffset() * 60 * 1000).toISOString().split('T')[0]}`)
+      .then(response => {
+        console.log("id___" +response.data.id);
+        const {id} = response.data;
+        setCalendarid(id);
+      })
+      .catch(error => {
+        const payload = { user: route.params.user, date: new Date(new Date() -  new Date().getTimezoneOffset() * 60 * 1000).toISOString().split('T')[0]};
+        console.log(payload);
+          axios.post(`/api/calendar/`,payload).then(response => {
+            const {id} = response.data;
+            // console.log("created calendar"+route.params.date);
+            setCalendarid(id);
+            console.log(id);
+          }
+          ).catch(error => console.log("inner:::"+error));
+      });
+      BackgroundGeolocation.ready({
+        distanceFilter: 500,
+        stopOnTerminate: false,
+        startOnBoot: true, 
+        logLevel: BackgroundGeolocation.LOG_LEVEL_VERBOSE,
+      }, (state) => {
+        console.log("- BackgroundGeolocation is ready: ");
+
+        if (!state.enabled) {
+          BackgroundGeolocation.start(function() {
+            console.log("- Start success");
+          });
+      }});
+
+
+      
+}, []);
+
+useEffect(() => {
+  console.log("useEffect"+calendarid);
+  Geolocation.getCurrentPosition(
+    position => {
+      const {latitude, longitude} = position.coords;
+      const payload = { calendar: calendarid, mpoint:"MULTIPOINT ("+longitude+" "+latitude+")"};
+      axios.post(`/api/location/`,payload).then(response => {
+        console.log("updated location");
+      }).catch(error => console.log("post error:::"+error));
+    },
+    error => {
+      console.log(error.code, error.message);
+    },
+    {enableHighAccuracy: false, timeout: 15000, maximumAge: 10000},
+  );
+  AppState.addEventListener('change', handleAppStateChange);
+  // return () => {
+  //     AppState.removeEventListener('change', handleAppStateChange);
+  // };
+
+  console.log("timeout");
+  const timer = setTimeout(Geolocation.getCurrentPosition(
+    position => {
+      const {latitude, longitude} = position.coords;
+      const payload = { calendar: calendarid, mpoint:"MULTIPOINT ("+longitude+" "+latitude+")"};
+      axios.put(`/api/location/update/${calendarid}`,payload).then(response => {
+        console.log("updated location");
+      }).catch(error => console.log("post error:::"+error));
+    },
+    error => {
+      console.log(error.code, error.message);
+    },
+    {enableHighAccuracy: false, timeout: 15000, maximumAge: 10000},
+  ), 60 * 1000);
+
+  return () => clearTimeout(timer);
+  
+}, [calendarid]);
+
+
+const onLocation = (location) => {
+  // console.log("latlngs"+latlngs);
+  console.log("onlocation"+calendarid+location);
+  if(calendarid!=0){
+    const payload = { calendar: calendarid, mpoint:"MULTIPOINT ("+location.coords.longitude+" "+location.coords.latitude+")"};
+    axios.put(`/api/location/update/${calendarid}`,payload).then(response => {
+      console.log("updated location");
+    }).catch(error => console.log("post error:::"+error));
+  }
+  }
+
+
+const onError = (error) => {
+  console.warn('[location] ERROR -', error);
+}
+
+
     LocaleConfig.locales.jp = {
         monthNames: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
         monthNamesShort: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
@@ -17,8 +172,20 @@ const CalendarDisplay = ({navigation, route}) => {
     const handleDayPress = (day) => {
         setSelected(day.dateString);
         console.log(new Date(day.dateString).toLocaleString());
-        navigation.navigate("Map",{date:new Date(day.dateString).toISOString().split('T')[0],user:route.params.user,email:route.params.email});
-        }
+        // navigation.navigate("Map",{date:new Date(day.dateString).toISOString().split('T')[0],user:route.params.user,email:route.params.email});
+
+        axios.get(`/api/calendar/${route.params.user}/?search=${day.dateString}`)
+      .then(response => {
+        const {title,description} = response.data;
+        console.log("title:"+title+"description:"+description);
+        navigation.navigate("BottomTab", { screen: "Schedule" ,date:new Date(day.dateString).toISOString().split('T')[0],user:route.params.user,email:route.params.email, title:title, description:description,not_created:false});
+        // navigation.navigate("Schedule",{date:new Date(day.dateString).toISOString().split('T')[0],user:route.params.user,email:route.params.email, title:title, description:description,not_created:false});
+      })
+      .catch(error => { 
+        navigation.navigate("BottomTab", { screen: "Schedule" ,date:new Date(day.dateString).toISOString().split('T')[0],user:route.params.user,email:route.params.email, title:"", description:"", not_created:true});
+        // navigation.navigate("Schedule",{date:new Date(day.dateString).toISOString().split('T')[0],user:route.params.user,email:route.params.email, title:"", description:"", not_created:true});
+      });
+    }
         
     return(
     <View style={styles.container}>
@@ -34,8 +201,24 @@ const CalendarDisplay = ({navigation, route}) => {
             selectedTextColor: 'white'
           }
          }}
+        onPressArrowLeft={(subtractMonth) => {
+          subtractMonth();
+          setCurrentDate(currentDate.add(-1, 'month'));
+        }}
+        onPressArrowRight={(addMonth) => {
+          addMonth();
+          setCurrentDate(currentDate.add(1, 'month'));
+        }}
+        theme={theme}
         style={styles.calendar}
         onDayPress={handleDayPress}
+        dayComponent={({ date }) => {
+          return (
+            <TouchableOpacity onPress={() => {handleDayPress(date)}} style={styles.button}>
+              <Text style={dayTextStyle(date, currentDate).dayText}> {date.day}</Text>
+            </TouchableOpacity>
+          );
+        }}
       />
         {/* <View/> */}
         <Text>DetailScreen</Text>
@@ -57,9 +240,31 @@ const CalendarDisplay = ({navigation, route}) => {
             navigation.navigate('Register');
         }}
         />
+        <Button
+        title="ScheduleDisplay画面に遷移する"
+        onPress={() => {
+            navigation.navigate("BottomTab", { screen: "Schedule" ,user:route.params.user, date:route.params.date});
+            navigation.navigate('Schedule',{user:route.params.user, date:route.params.date});
+        }}
+        />
   </View>
 );
 };
+const dayTextStyle = (date, currentDate) =>
+  StyleSheet.create({
+    dayText: {
+      fontFamily:"TrebuchetMS-Bold",
+      fontSize: 20,
+      borderWidth: 1,
+      borderRadius: 1,
+      paddingLeft: String(date.day).length == 2 ? 5 : 10,
+      paddingTop: 15,
+      height: "100%",
+      width: "100%",
+      backgroundColor: date.dateString === moment().format('YYYY-MM-DD') ? '#CCCCCC' : 'transparent',
+      color: date.month !== currentDate.month() + 1 ? 'gray' :  moment(date.dateString).days() === 0 ? 'red' : moment(date.dateString).days() === 6 ? 'blue' : 'black',
+    },
+  });
 
 const styles = StyleSheet.create({
 container: {
@@ -70,12 +275,72 @@ container: {
   
 },
 calendar: {
-  width: 380,
-  height: 364,
+  fontfamily:"TrebuchetMS-Bold",
+  width: 370,
+  height: 470,
   borderWidth: 1,
   borderColor: 'gray',
   borderRadius: 10,
 },
+header: {
+  fontFamily:"TrebuchetMS-Bold",
+  fontSize: 100,
+},
 });
+
+theme = {
+  'stylesheet.calendar.header': {
+      header: {
+        // override the default header style react-native-calendars/src/calendar/header/style.js
+        backgroundColor: 'gray', // set the backgroundColor for header
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingVertical: 15,
+        width: 370,
+        right: 6,
+        borderTopLeftRadius: 8,
+        borderTopRightRadius: 8,
+      },
+      monthText: {
+        color: '#fff',
+        fontWeight: '700',
+        fontSize: 20,
+        fontFamily:"TrebuchetMS-Bold",
+      },
+      dayHeader: {
+        marginTop: 13,
+        // marginBottom: 7,
+        // width: 30,
+        textAlign: 'center',
+        fontSize: 18,
+        bottom: 10,
+        color: 'gray',
+        fontFamily:"TrebuchetMS-Bold",
+      },
+    },
+    'stylesheet.calendar.main': {
+      monthView: {
+        flex: 1,
+        height: '100%',
+        justifyContent: 'space-around',
+        fontfamily:"TrebuchetMS-Bold",
+      },
+      week: {
+        flex: 1,
+        marginVertical: 0,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        fontfamily:"TrebuchetMS-Bold",
+      },
+      dayContainer: {
+        borderColor: '#f5f5f5',
+        borderWidth: 1,
+        // backgroundColor: 'pink',
+        flex:1,
+        fontfamily:"TrebuchetMS-Bold",
+      },
+    },
+  }
 
 export default CalendarDisplay
---
+++ components/schedule/ScheduleDisplay.js
@@ -0,0 +1,77 @@
+import axios from 'axios';
+import React, { Component , useState,useEffect} from 'react';
+import { View, Text, TextInput,StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/AntDesign';
+
+const ScheduleDisplay = ({navigation, route}) => {
+    const [title, setTitle] = useState(route.params.title);
+    const [description, setDescription] = useState(route.params.description);
+    const loadData = () =>{
+        axios.get()
+    }
+    useEffect(() => {
+      setTitle(route.params.title);
+      setDescription(route.params.description);
+
+    }
+    ,[route.params.title,route.params.description]);
+
+  return (
+    <View style={styles.parent}>
+        <Text style={styles.title}>タイトル<Icon style={styles.icon1} name="edit" size={25} onPress={()=>navigation.navigate("EditModal",{user:route.params.user, date:route.params.date,title:title,description:description, text:"タイトル", not_created:route.params.not_created})}/></Text>
+      <Text style={styles.title_body}>{title}</Text>
+        <Text style={styles.description}>説明 <Icon style={styles.icon2} name="edit" size={25} onPress={()=>navigation.navigate("EditModal",{user:route.params.user, date:route.params.date ,title:title,description:description, isMultiline:true, text:"説明",not_created:route.params.not_created})}/></Text>
+      <Text style={styles.description_body}>{description }</Text>
+      </View>
+    
+  )
+}
+const styles = StyleSheet.create({
+  parent:{
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    
+  },
+  title: {
+    top:20,
+    fontSize: 18,
+    fontFamily: 'TrebuchetMS-Bold',
+  },
+  title_body: {
+    width: 350,
+    paddingTop: 20,
+    paddingBottom: 20,
+    top:40,
+    fontSize: 25,
+    borderWidth:2,
+    fontFamily: 'TrebuchetMS-Bold',
+
+  },
+  icon1: {
+    position: "absolute",
+    bottom: 715,
+    left: 240
+  },
+  description: {
+    top: 60,
+    fontSize: 18,
+    fontFamily: 'TrebuchetMS-Bold',
+  },
+  description_body: {
+    width: 350,
+    paddingTop: 10,
+    paddingBottom:350,
+    top: 80,
+    fontSize: 20,
+    borderWidth:2,
+    fontFamily: 'TrebuchetMS-Bold',
+  },
+  icon2: {
+    position:"absolute",
+    bottom: 595,
+    left: 215,
+  },
+});
+
+export default ScheduleDisplay

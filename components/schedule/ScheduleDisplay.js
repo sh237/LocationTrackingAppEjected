77f1889056678591493c729ea5e7c91361a3b494
Shunya Nagashima <syun864297531@gmail.com>
2022-09-30 18:13:16 +0900

    スタイルの追加

--- components/schedule/ScheduleDisplay.js
+++ components/schedule/ScheduleDisplay.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import React, { Component , useState,useEffect} from 'react';
 import { View, Text, TextInput,StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/AntDesign';
+import onForegroundLocation from '../function/onForegroundLocation';
 
 const ScheduleDisplay = ({navigation, route}) => {
     const [title, setTitle] = useState(route.params.title);
@@ -16,11 +17,18 @@ const ScheduleDisplay = ({navigation, route}) => {
     }
     ,[route.params.title,route.params.description]);
 
+    useEffect(() => {
+      // const timer = setTimeout(()=>onForegroundLocation(route.params.calendarid), 60 * 1000);
+      // return () => clearTimeout(timer);
+
+    }, []);
+
+
   return (
     <View style={styles.parent}>
-        <Text style={styles.title}>タイトル<Icon style={styles.icon1} name="edit" size={25} onPress={()=>navigation.navigate("EditModal",{user:route.params.user, date:route.params.date,title:title,description:description, text:"タイトル", not_created:route.params.not_created})}/></Text>
+        <Text style={styles.title}>タイトル<Icon style={styles.icon1} name="edit" size={25} onPress={()=>navigation.navigate("EditModal",{user:route.params.user, date:route.params.date,title:title,description:description, text:"タイトル", not_created:route.params.not_created,theme_color:route.params.theme_color})}/></Text>
       <Text style={styles.title_body}>{title}</Text>
-        <Text style={styles.description}>説明 <Icon style={styles.icon2} name="edit" size={25} onPress={()=>navigation.navigate("EditModal",{user:route.params.user, date:route.params.date ,title:title,description:description, isMultiline:true, text:"説明",not_created:route.params.not_created})}/></Text>
+        <Text style={styles.description}>説明 <Icon style={styles.icon2} name="edit" size={25} onPress={()=>navigation.navigate("EditModal",{user:route.params.user, date:route.params.date ,title:title,description:description, isMultiline:true, text:"説明",not_created:route.params.not_created,theme_color:route.params.theme_color})}/></Text>
       <Text style={styles.description_body}>{description }</Text>
       </View>
     
